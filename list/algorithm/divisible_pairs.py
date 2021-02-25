ba7eeaae7fe222fd2e4587f11f4645fb0b4d1f26
tojimahammatov <tojimahammatov@gmail.com>
2021-02-25 17:49:51 +0900

    create divisible_pairs.py

---
+++ list/algorithm/divisible_pairs.py
@@ -0,0 +1,7 @@
+'''
+    Given is a list containing durations of songs (in seconds),
+    task is to find a number of pairs whose total duration is divisible by 60.
+
+    (LeetCode Rumours: online assessment problem at Amazon)
+'''
+
