2d5974a24297809c3004121373239512713ea8f6
Sherzodbek <tojimahammatov@gmail.com>
2022-01-06 09:09:48 +0500

    add implementation logic

--- list/min_cost_climbing_stars.py
+++ list/min_cost_climbing_stars.py
@@ -5,3 +5,14 @@ You can either start from the step with index 0, or the step with index 1.
 
 Return the minimum cost to reach the top of the floor.
 '''
+
+def minCostClimbingStars(cost):
+    top = len(cost)
+    payment = [0] * top
+    payment[0], payment[1] = cost[0], cost[1]
+    # each index's payment would be a cost of this index plus minimum of two previous payments
+    for i in range(2, top):
+        payment[i] = cost[i] + min(payment[i-1], payment[i-2])
+    # and there are two ways to reach top at the end: through top-1 index or top-2 index, 
+    #       and we choose minimum among them
+    return min(payment[top-1], payment[top-2])
