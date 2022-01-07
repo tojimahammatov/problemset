041c23dc88d602c0c8438718bbb65121da23f5b8
Sherzodbek <tojimahammatov@gmail.com>
2022-01-07 15:23:46 +0500

    add function implementation

--- list/middle_node.py
+++ list/middle_node.py
@@ -2,3 +2,19 @@
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
 '''
+
+# class ListNode:
+#     def __init__(self, value, next) -> None:
+#         self.value = value 
+#         self.next = next 
+
+# two pointers are good to go, we move twice as we preserve middle by moving one
+def middle_node(head):
+    slow, fast = head, head
+    while fast:
+        if fast.next == None:
+            return slow
+        else:
+            slow = slow.next
+            fast = fast.next.next
+    return slow
